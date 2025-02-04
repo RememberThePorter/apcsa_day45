1eeadbf65a4d119259a4854a7f2a6dc5e8c99063
RememberThePorter <connor.james.hanson@gmail.com>
2025-02-04 12:44:50 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Update this file with your own code.
+class Main {
+    public static void main(String[] args) {
+        // The line below runs tests on your classes before your code starts. Change
+        // the string to "Extra Credit" once your program passes part A if you wish to
+        // pursue extra points.
+        TestRunner.Run("Part A");
+
+        // TODO: Read the Student guide before getting started! Then, add your code here, starting with Checkpoint 1.
+
+    }
+}
