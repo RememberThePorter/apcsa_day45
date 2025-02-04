0d0b4e5f5a4e7121453b6827624459719cf4e0cb
RememberThePorter <connor.james.hanson@gmail.com>
2025-02-04 15:46:00 -0800

    Initial commit

---
+++ src/RememberThePorter/TextCell.java
@@ -0,0 +1,15 @@
+package RememberThePorter;
+
+import RememberThePorter.Interfaces.Cell;
+
+public class TextCell implements Cell {
+    @Override
+    public String abbreviatedCellText() {
+        return "";
+    }
+
+    @Override
+    public String fullCellText() {
+        return "";
+    }
+}
