a60008c833f8356921abafaad63f20e3111038fe
RememberThePorter <connor.james.hanson@gmail.com>
2025-05-16 14:42:09 -0700

    Main.java now runs TextExcel.java; deleted TestRunner.java and TestsALL.java

--- src/RememberThePorter/Main.java
+++ src/RememberThePorter/Main.java
@@ -1,16 +1,7 @@
 package RememberThePorter;
 
-import RememberThePorter.Tests.TestRunner;
-
-// Update this file with your own code.
 class Main {
     public static void main(String[] args) {
-        // The line below runs tests on your classes before your code starts. Change
-        // the string to "Extra Credit" once your program passes part A if you wish to
-        // pursue extra points.
-        TestRunner.Run("Part A");
-
-        // TODO: Read the Student guide before getting started! Then, add your code here, starting with Checkpoint 1.
-
+        TextExcel.main(args);
     }
 }
