f2ccd8e892d841dc24dc3d2d8a673cc2780cce5a
RememberThePorter <connor.james.hanson@gmail.com>
2025-05-16 17:18:56 -0700

    Added greeting upon launch

--- src/RememberThePorter/Main.java
+++ src/RememberThePorter/Main.java
@@ -2,6 +2,7 @@ package RememberThePorter;
 
 class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to TextExcel! Enter \"help\" to see a list of commands you can use.");
         TextExcel.main(args);
     }
 }
